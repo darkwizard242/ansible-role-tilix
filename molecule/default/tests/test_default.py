484dafa4d7ffebd348a69eb88abfd02d59b1c870
Ali Muhammad <40025815+darkwizard242@users.noreply.github.com>
2020-01-26 00:29:30 -0500

    set assertion for molecule testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,21 +6,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-TILIX_BINARY_PATH = '/usr/bin/tilix'
-TILIX_PACKAGE = 'tilix'
-
-
 def test_tilix_package_installed(host):
-    host.package("TILIX_PACKAGE").is_installed
+    assert host.package("tilix").is_installed
 
 
 def test_tilix_binary_exists(host):
-    host.file('TILIX_BINARY_PATH').exists
+    assert host.file('/usr/bin/tilix').exists
 
 
 def test_tilix_binary_file(host):
-    host.file('TILIX_BINARY_PATH').is_file
+    assert host.file('/usr/bin/tilix').is_file
 
 
-def test_tilix_binary_whereis(host):
-    host.check_output('whereis tilix') == TILIX_BINARY_PATH
+def test_tilix_binary_which(host):
+    assert host.check_output('which tilix') == '/usr/bin/tilix'
